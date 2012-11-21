1224f2b1baebca6f64961fbdeb589c480b8c836a
Michael GUIDET <guidet_m@tumbleweed-studio.net>
2012-11-21 15:25:02 +0000

    Ajout de code dans la classe SlimeGame.js

--- SlimeGame/SlimeGame.js
+++ SlimeGame/SlimeGame.js
@@ -7,27 +7,53 @@
  */
 
 function SlimeGame(canvasObject) {
+	//Set the starting values
 	this.ia_score = 0;
 	this.player_score = 0;
+
+	//Load the canvas in the object
 	this.canvas = canvasObject;
+
+	//Create the image loader that will load all the images
 	this.imageLoader = new ImageLoader();
+
+	//Create the window which will contain the game
 	this.win = new TW.Graphic.Window(this.canvas);
+
+	//Create the layer which will contain the different element
 	this.layer = new TW.Graphic.Layer({width:800, height:600});
+
+	//Create the event provider. This will ensure that the key are pressed or not
 	this.keyboard = new TW.Event.KeyboardInput();
+
+	//Create the object SlimeBall
 	this.ball = new SlimeBall(150, 150, 0, 775, 375);
+
+	//Create the player and the ia through the object SlimePlayer
 	this.player = new SlimePlayer(100, 350, 0, 290, "PLAYER");
 	this.ia = new SlimePlayer(600, 350, 410, 700, "IA");
+
+	//Create the object SlimeNet
 	this.net = new SlimeNet(390, 325, 20, 100);
+
+	//Create the sprite of the background and the score
 	this.background = new TW.Graphic.Sprite({x:0, y:0, width:800, height:600});
 	this.sprite_score_ai = new TW.Graphic.Sprite({x:431, y:20, width:369, height:42});
 	this.sprite_score_player = new TW.Graphic.Sprite({x:0, y:20, width:369, height:42});
 }
 
 SlimeGame.prototype.initialize = function() {
+	//Start the image loader
 	this.imageLoader.initialize();
+
+	//Load the images
 	this.imageLoader.loadImages();
+
+	//Create the collision for the different object
 	this.player.collision = new TW.Collision.CollisionCircle(250, 400, 50);
 	this.ia.collision = new TW.Collision.CollisionCircle(650, 400, 50);
+
+	//Set the images to each object
 	this.player.sprite.setImage(this.imageLoader.getImageByName("yellow-normal"));
 	this.ia.sprite.setImage(this.imageLoader.getImageByName("red-normal"));
 	this.net.sprite.setImage(this.imageLoader.getImageByName("net"));
@@ -35,6 +61,8 @@ SlimeGame.prototype.initialize = function() {
 	this.background.setImage(this.imageLoader.getImageByName("background"));
 	this.sprite_score_ai.setImage(this.imageLoader.getImageByName("red-0"));
 	this.sprite_score_player.setImage(this.imageLoader.getImageByName("yellow-0"));
+
+	//Adding of the different sprite in the layer
 	this.layer.addChild(this.background);
 	this.layer.addChild(this.net.sprite);
 	this.layer.addChild(this.ia.sprite);
@@ -42,16 +70,21 @@ SlimeGame.prototype.initialize = function() {
 	this.layer.addChild(this.ball.sprite);
 	this.layer.addChild(this.sprite_score_ai);
 	this.layer.addChild(this.sprite_score_player);
+
+	//Adding the layer to the window
 	this.win.addChild(this.layer);
 };
 
 SlimeGame.prototype.draw = function() {
+
+	//Call the window draw function
 	this.win.draw();
-	this.player.collision.draw(this.win.getCanvas());
 };
 
 SlimeGame.prototype.score = function () {
 
+	//Looking at the position of the ball in order to know who is scoring
+	//Change the score and reset all the setting of the ball
 	if (this.ball.position.x < 400) {
 		this.ia_score++;
 		this.ball.reset(this.ia.collision.x);
@@ -59,13 +92,18 @@ SlimeGame.prototype.score = function () {
 		this.player_score++;
 		this.ball.reset(this.player.collision.x);
 	}
+
+	//When a player reach 8 point, reset the score
 	if (this.ia_score === 8 || this.player_score === 8) {
 		this.ia_score = 0;
 		this.player_score = 0;
 	}
+
+	//Load the images for the current score
 	this.sprite_score_player.setImage(this.imageLoader.getImageByName("yellow-" + this.player_score));
 	this.sprite_score_ai.setImage(this.imageLoader.getImageByName("red-" + this.ia_score));
 
+	//Load the images for the slime face depending on the score
 	if (this.ia_score > this.player_score) {
 		this.player.sprite.setImage(this.imageLoader.getImageByName("yellow-bad"));
 		this.ia.sprite.setImage(this.imageLoader.getImageByName("red-happy"));
@@ -79,10 +117,15 @@ SlimeGame.prototype.score = function () {
 };
 
 SlimeGame.prototype.update = function() {
+
+	//Call the different update function that have to be used
 	this.player.update(this.keyboard);
 	this.ia.update(this.ball);
 	this.ball.update(this.player.collision, this.ia.collision, this.net.collision);
+
+	//Check if the ball touch the ground
 	if (this.ball.position.y > this.ball.y_max) {
+		//Call the score function that will change the score and the settings of the ball.
 		this.score();
 	}
 }
